06148e49cf0970be4604675276ecfe4627d550df
Andrii <a.shyrshov@gmail.com>
2021-03-23 22:52:27 +0200

    1

---
+++ src/com/company/Human.java
@@ -0,0 +1,18 @@
+package com.company;
+
+class Human {
+    double weight;
+    double age;
+    String firstName;
+    String lastName;
+    double height;
+    public void newHuman(double weight, double age, String firstName, String lastName, double height){
+        this.weight = weight;
+        this.age = age;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.height =height;
+
+    }
+
+}
