78283211cecf26a28a4b2a5c7953d6ef9cea0db8
Andrii <a.shyrshov@gmail.com>
2021-03-24 18:29:34 +0200

    3-3

--- src/com/company/Wheels.java
+++ src/com/company/Wheels.java
@@ -23,6 +23,8 @@ class Car {
     public static void main(String[] args) {
         Garage garage = new Garage();
         garage.addPlace(3);
+        //делал по инфе:https://www.sitesbay.com/java/java-relationship-in-java#:~:text=A%20method%20of%20one%20class,memory%20space%20will%20be%20destroyed.
+        //потом удалить комменты
     }
 }
 
