8258c80ded5100a3f20c2054a144ea758acfde59
Andrii <a.shyrshov@gmail.com>
2021-03-26 18:43:04 +0200

    Overloaded

---
+++ src/com/company/Overloaded.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Overloaded {
+    int firstVar;
+    double secondVar;
+    int thirdVar;
+
+    public Overloaded (int firstVar, double secondVar, int thirdVar) {
+        this.firstVar = firstVar;
+        this.secondVar = secondVar;
+        this.thirdVar = thirdVar;
+    }
+
+    public double Sum (int firstVar, double secondVar) {
+        return firstVar + secondVar;
+    }
+
+    public double Sum (int firstVar, int thirdVar) {
+        return firstVar + secondVar;
+    }
+
+    public double Sum (double secondVar, int thirdVar) {
+        return secondVar + thirdVar;
+    }
+
+}
